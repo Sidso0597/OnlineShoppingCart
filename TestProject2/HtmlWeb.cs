a9130a2428b3982d827c4ff05b6afff78d0d5174
SiddhiKamat <kamatsiddhi97@gmail.com>
2021-02-17 01:17:19 +0530

    third commit

---
+++ TestProject2/HtmlWeb.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TestProject2
+{
+    internal class HtmlWeb
+    {
+        public HtmlWeb()
+        {
+        }
+
+        internal object Load(string url)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
